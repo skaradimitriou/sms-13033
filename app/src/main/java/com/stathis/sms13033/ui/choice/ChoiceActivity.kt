c7ba829805d943054264f5c494352de81d57bb9e
Stathis Karaimitriou <stathis.karadimitriou@gmail.com>
2020-11-10 23:38:17 +0200

    SMS Manager & Request Code

--- app/src/main/java/com/stathis/sms13033/ui/choice/ChoiceActivity.kt
+++ app/src/main/java/com/stathis/sms13033/ui/choice/ChoiceActivity.kt
@@ -1,41 +1,96 @@
 package com.stathis.sms13033.ui.choice
 
-import android.content.Intent
-import android.net.Uri
+import android.Manifest
+import android.app.Activity
+import android.content.pm.PackageManager
+import android.util.Log
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.lifecycle.ViewModelProvider
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.stathis.sms13033.R
 import com.stathis.sms13033.abstraction.AbstractActivity
 import com.stathis.sms13033.listeners.ChoiceActivityListener
 import com.stathis.sms13033.ui.choice.model.MovementOption
 import kotlinx.android.synthetic.main.activity_choice.*
-import kotlinx.android.synthetic.main.activity_user.*
 
-class ChoiceActivity : AbstractActivity(R.layout.activity_choice),ChoiceActivityListener{
+class ChoiceActivity : AbstractActivity(R.layout.activity_choice), ChoiceActivityListener {
 
-    private lateinit var fullName : String
-    private lateinit var address : String
-    private lateinit var viewModel : ChoiceViewModel
+    private lateinit var fullName: String
+    private lateinit var address: String
+    private val SEND_SMS_REQ_CODE = 1
+    private lateinit var viewModel: ChoiceViewModel
 
     override fun initLayout() {
         viewModel = ViewModelProvider(this).get(ChoiceViewModel::class.java)
     }
 
     override fun runOperation() {
+
         fullName = intent.getStringExtra("fullName") ?: ""
         address = intent.getStringExtra("address") ?: ""
 
         choices_recycler.adapter = viewModel.adapter
         viewModel.initActivityListener(this)
+
     }
 
     override fun stopOperation() {
         //
     }
 
-    override fun sendSMS(movementOption: MovementOption) {
-        val intent = Intent(Intent.ACTION_SENDTO, Uri.parse("smsto:13033"))
-        intent.putExtra("sms_body", "ΜΕΤΑΚΙΝΗΣΗ ${movementOption.movementId} $fullName $address")
-        startActivity(intent)
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        when (requestCode) {
+            SEND_SMS_REQ_CODE -> {
+                if (grantResults.size > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(this, "Permission granted", Toast.LENGTH_SHORT).show()
+                } else {
+                    Toast.makeText(this, "Permission denied", Toast.LENGTH_SHORT).show()
+                }
+            }
+            else -> {
+                super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+            }
+        }
     }
+
+    override fun sendSMS(movementOption: MovementOption) {
+        val permission = ContextCompat.checkSelfPermission(this, Manifest.permission.SEND_SMS)
+
+        if (permission == PackageManager.PERMISSION_GRANTED) {
+            sendSMSMessage(movementOption)
+        } else {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.SEND_SMS),
+                SEND_SMS_REQ_CODE
+            )
+        }
+
+    }
+
+    private fun sendSMSMessage(movementOption: MovementOption) {
+        val builder = MaterialAlertDialogBuilder(this)
+        builder.setTitle(R.string.movement_reason_sure)
+        builder.setMessage("Έχετε επιλέξει SMS για: ${movementOption.movementName}. Είστε σίγουρος/η;")
+            .setPositiveButton("Αποστολή") { dialog, id ->
+
+                //sending SMS
+                viewModel.sendSMSMessage(movementOption, fullName, address)
+                viewModel.showSuccessMessage(this)
+            }
+            .setNegativeButton(
+                "Άκυρο"
+            ) { dialog, id ->
+                dialog.dismiss()
+            }
+        builder.show()
+    }
+
 
 }
--- app/src/main/java/com/stathis/sms13033/ui/choice/ChoiceViewModel.kt
+++ app/src/main/java/com/stathis/sms13033/ui/choice/ChoiceViewModel.kt
@@ -1,26 +1,32 @@
 package com.stathis.sms13033.ui.choice
 
+import android.content.Context
+import android.telephony.SmsManager
+import android.util.Log
 import androidx.lifecycle.ViewModel
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.stathis.sms13033.R
 import com.stathis.sms13033.listeners.ChoiceActivityListener
 import com.stathis.sms13033.listeners.ItemClickListener
 import com.stathis.sms13033.ui.choice.model.MovementOption
 import com.stathis.sms13033.ui.choice.recycler.MovementOptionsAdapter
 import com.stathis.sms13033.ui.choice.repository.ChoiceRepo
+import java.lang.Exception
 
 class ChoiceViewModel : ViewModel(), ItemClickListener {
 
     val adapter = MovementOptionsAdapter(this)
     private lateinit var choiceActivityListener: ChoiceActivityListener
 
-    init{
+    init {
         getMovementOptions()
     }
 
-    fun initActivityListener(choiceActivityListener: ChoiceActivityListener){
+    fun initActivityListener(choiceActivityListener: ChoiceActivityListener) {
         this.choiceActivityListener = choiceActivityListener
     }
 
-    private fun getMovementOptions(){
+    private fun getMovementOptions() {
 
         val choiceRepo = ChoiceRepo()
         adapter.submitList(choiceRepo.getMovementChoices())
@@ -28,6 +34,32 @@ class ChoiceViewModel : ViewModel(), ItemClickListener {
 
     override fun itemClicked(movementOption: MovementOption) {
         choiceActivityListener.sendSMS(movementOption)
+    }
+
+    fun sendSMSMessage(movementOption: MovementOption, fullName: String, address: String) {
+        try {
+            val sms = SmsManager.getDefault()
+            sms.sendTextMessage(
+                "13033",
+                null,
+                "${movementOption.movementId} $fullName $address",
+                null,
+                null
+            )
+        } catch (e: Exception) {
+            Log.d("e", e.toString())
+        }
+    }
+
+    fun showSuccessMessage(context: Context) {
+        val builder = MaterialAlertDialogBuilder(context)
+        builder.setTitle(R.string.movement_reason_sure)
+        builder.setMessage("Το sms έχει σταλεί. Επιβεβαιώστε οτι λάβατε απάντηση.")
+        builder.show()
+    }
+
+    fun getMovementOptionSelected(){
+
     }
 
 }
